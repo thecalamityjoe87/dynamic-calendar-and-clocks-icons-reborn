2fec3880aa526e2bf4d227676a59fe5bd34531df
Fraxinus Acer <yasson2004@gmail.com>
2025-09-25 17:00:48 +0300

    Fix Russian translation

--- po/update_strings.sh
+++ po/update_strings.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SCRIPTDIR=`dirname $0`
-xgettext  --from-code=UTF-8 -k_ -kN_  -o dynamic-calendar-and-clocks-icons.pot "$SCRIPTDIR"/../dynamic-calendar-and-clocks-icons@thecalamityjoe87.github.com/*.js "$SCRIPTDIR"/../dynamic-calendar-and-clocks-icons@thecalamityjoe87.github.com//schemas/*.xml
+xgettext  --from-code=UTF-8 -k_ -kN_  -o dynamic-calendar-and-clocks-icons.pot "$SCRIPTDIR"/../dynamic-calendar-and-clocks-icons-reborn@thecalamityjoe87.github.com/*.js "$SCRIPTDIR"/../dynamic-calendar-and-clocks-icons-reborn@thecalamityjoe87.github.com/schemas/*.xml
 
 for fn in *.po; do
 	msgmerge -U "$fn" dynamic-calendar-and-clocks-icons.pot
